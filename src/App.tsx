8e37e6a5b1a3e7aa019d474bfe8c8dfd6e3cf29b
Mr Mashayekhi <mm.mashaekhi1988@gmail.com>
2026-07-02 18:21:43 +0330

    directories  build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Login from "./pages/Login";
+
 function App() {
-  return <h1>React Admin Dashboard</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+
+      <Route path="/users" element={<Users />} />
+
+      <Route path="/settings" element={<Settings />} />
+
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/Users.tsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <h1>Users</h1>;
+}
+
+export default Dashboard;
